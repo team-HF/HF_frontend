1ba596ba6330241c5b3d2e293a8202c88bab27da
Namhyun <jenple9709@gmail.com>
2024-09-08 17:07:35 +0900

    Design: information 모달 디자인

---
+++ src/pages/my-page/components/information-modal.style.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 20.625rem;
+  height: 13.625rem;
+  border-radius: 0.625rem;
+  padding: 0.625rem;
+  box-shadow: 0px 0px 10px 0px #00000040;
+`;
+
+export const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 0.625rem;
+  gap: 0.5rem;
+`;
+export const InformationContainer = styled.div<{ title: string }>`
+  background-color: ${({ title, theme }) =>
+    title === '고수' ? theme.colors.main : theme.colors.sub};
+  display: flex;
+  flex-direction: row;
+  width: 18rem;
+  height: 4.375rem;
+  border-radius: 0.625rem;
+`;
+
+export const TitleContainer = styled.div<{ title: string }>`
+  display: flex;
+  color: ${({ title }) => (title === '고수' ? '#FFFFFF' : '#000000')};
+  align-items: center;
+  justify-content: center;
+  width: 4rem;
+  font-size: 0.8125rem;
+  font-weight: 700;
+`;
+
+export const DescriptionContainer = styled.div<{ title: string }>`
+  display: flex;
+  color: ${({ title }) => (title === '고수' ? '#FFFFFF' : '#000000')};
+  padding: 0.75rem;
+  font-weight: 400;
+  font-size: 0.875rem;
+  align-items: center;
+  line-height: 150%;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  margin-left: auto;
+`;
+export const XIcon = styled.img`
+  width: 0.875rem;
+  height: 0.875rem;
+  cursor: pointer;
+`;
