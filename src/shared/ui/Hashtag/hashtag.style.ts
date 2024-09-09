62e69277711fff83adcd88976b2ecbbe725bedeb
Namhyun <jenple9709@gmail.com>
2024-09-09 23:01:48 +0900

    design: Hashtag 컴포넌트

---
+++ src/shared/ui/Hashtag/hashtag.style.ts
@@ -0,0 +1,16 @@
+import { styled } from 'styled-components';
+
+export const HashtagBox = styled.div`
+  display: flex;
+  width: 3.75rem;
+  height: 1.125rem;
+  border-radius: 0.5625rem;
+  background-color: #99999999;
+  font-size: 0.625rem;
+  line-height: 0.625rem;
+  align-items: center;
+  justify-content: center;
+  padding: 0 0.25rem;
+  width: auto;
+  white-space: nowrap;
+`;
