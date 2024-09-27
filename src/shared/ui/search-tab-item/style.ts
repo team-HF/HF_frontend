bfb45e829951cbe8d58bd7454176314781de56a2
Namhyun <jenple9709@gmail.com>
2024-09-27 16:34:19 +0900

    Design: search-tab-item

---
+++ src/shared/ui/search-tab-item/style.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const SearchTabContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 2.875rem;
+  height: 2.75rem;
+  padding: 0.625rem;
+  top: 1.25rem;
+  left: 1.25rem;
+  gap: 10px;
+  cursor: pointer;
+
+  &:hover {
+    border-image-source: ${({ theme }) => theme.colors.linear};
+    border-image-slice: 1;
+    border-bottom: 2px solid;
+  }
+`;
+
+export const TabText = styled.span`
+  font-size: 0.9375rem;
+  font-weight: 600;
+  line-height: 0.9375rem;
+  color: #888888;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.main};
+  }
+`;
