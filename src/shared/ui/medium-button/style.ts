92279079b54744a70e07b8f60d78b981f301c3f6
Namhyun <jenple9709@gmail.com>
2024-09-28 19:16:35 +0900

    Design: medium-button 디자인

---
+++ src/shared/ui/medium-button/style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+//disabled 속성 스타일은 고정
+export const StyledMediumButton = styled.button<{
+  backgroundColor: string;
+  disabled?: boolean;
+  border: string;
+}>`
+  background-color: ${({ backgroundColor, disabled }) =>
+    disabled ? '#F2F2F2' : backgroundColor};
+
+  width: 12.5rem;
+  height: 3rem;
+  border: ${({ border, disabled }) => (disabled ? 'none' : border)};
+  border-radius: 0.3125rem;
+`;
+
+export const StyledText = styled.p<{
+  color: string;
+  disabled?: boolean;
+}>`
+  color: ${({ theme, color, disabled }) =>
+    disabled ? theme.colors.gray5 : color};
+`;
