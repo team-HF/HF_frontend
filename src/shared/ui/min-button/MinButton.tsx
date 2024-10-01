0f63c8839a81f918e1f0164533e59852e947ef43
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2024-10-01 17:58:37 +0900

    feat: min-button UI 구현

---
+++ src/shared/ui/min-button/MinButton.tsx
@@ -0,0 +1,27 @@
+import * as S from "./style";
+
+type buttonShape = "square" | "around";
+
+type buttonStyle =
+  | "style_1"
+  | "style_2"
+  | "style_3"
+  | "style_4"
+  | "style_5"
+  | "style_6";
+
+export interface minButtonProps {
+  name: string;
+  button_shape: buttonShape;
+  button_style: buttonStyle;
+}
+
+const MinButton = ({ name, button_shape, button_style }: minButtonProps) => {
+  return (
+    <S.Container button_shape={button_shape} button_style={button_style}>
+      {name}
+    </S.Container>
+  );
+};
+
+export default MinButton;
