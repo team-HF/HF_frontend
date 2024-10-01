83d698c6ba94c874cced60118fb94391217eb650
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2024-10-01 21:40:40 +0900

    design: 최근 로그인 라벨 UI 구현

---
+++ src/shared/ui/login-marker/LoginMarker.tsx
@@ -0,0 +1,12 @@
+import * as S from "./style";
+
+const LoginMarker = () => {
+  return (
+    <S.Container>
+      <S.Title>최근 로그인</S.Title>
+      <S.Triangle/>
+    </S.Container>
+  );
+};
+
+export default LoginMarker;
