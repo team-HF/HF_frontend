e5150711bab4976b73d91430469fab04dbbc0d31
Namhyun <jenple9709@gmail.com>
2024-10-08 22:47:56 +0900

    Design: 하단 nav 변경된 디자인으로 수정 및 theme 그라데이션 컬러값 변경

--- src/shared/ui/footer-nav/FooterNav.tsx
+++ src/shared/ui/footer-nav/FooterNav.tsx
@@ -3,10 +3,10 @@ import * as S from './style';
 export default function FooterNav() {
   return (
     <S.Container>
-      <S.PencilIcon src="svg/pencil-icon.svg" alt="write" />
-      <S.HomeIcon src="svg/home-icon.svg" alt="home" />
-      {/* MyPageIcon 파일 수정해야 함 */}
-      <S.MyPageIcon src="svg/my-icon.png" alt="my-page" />
+      <S.HomeIcon src="svg/gnb-home-icon.svg" alt="home-icon" />
+      <S.ChatIcon src="svg/gnb-chat-icon.svg" alt="chat-icon" />
+      <S.WriteIcon src="svg/gnb-write-icon.svg" alt="write-icon" />
+      <S.MyPageIcon src="svg/gnb-profile-icon.svg" alt="profile-icon" />
     </S.Container>
   );
 }
