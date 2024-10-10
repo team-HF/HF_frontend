a7a71f17e96299a3e2e18f496021d09f67e8448b
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2024-10-11 01:07:07 +0900

    design: Login page 디자인 수정

--- src/pages/login/style.tsx
+++ src/pages/login/style.tsx
@@ -7,11 +7,6 @@ export const Container = styled.div`
   align-items: center;
   height: 100vh;
 `;
-export const LoginTitle = styled.h1`
-  position: relative;
-  top: 15vh;
-  ${theme.fontSize.title_1};
-`;
 export const BtnBox = styled.div`
   position: absolute;
   top: 50%;
@@ -19,24 +14,25 @@ export const BtnBox = styled.div`
   transform: translate(-50%, -50%);
   display: flex;
   flex-direction: column;
+  gap: 0.5rem;
+  width: 20rem;
+  @media (min-width: 768px) and (max-width: 1919px) {
+    width: 40rem;
+  }
+  @media (min-width: 1920px) {
+    width: 67.5rem;
+  }
 `;
 export const OauthBtn = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 342px;
-  height: 44px;
+  width: 100%;
+  height: 2.75rem;
   border: none;
-  border-radius: 10px;
-  color: ${theme.colors.black};
-  font-size: 0.938rem;
-  font-weight: 700;
-  line-height: 1.467rem;
-  letter-spacing: -0.01rem;
+  border-radius: 0.5rem;
   &.button_login_google {
-    border: 1px solid #c7c7c7;
-    background-color: ${theme.colors.white};
-    margin-bottom: 20px;
+    background-color: #474747;
   }
   &.button_login_kakao {
     background-color: #fae100;
@@ -49,8 +45,20 @@ export const LogoIcon = styled.div<{ src: string }>`
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
-  margin-right: 8px;
   &.kakao {
     height: 16px;
+  }
+`;
+export const BtnText = styled.span`
+  width: 15.75rem;
+  font-size: 0.938rem;
+  font-weight: 700;
+  line-height: 1.467rem;
+  letter-spacing: -0.01rem;
+  &.button_login_google {
+    color: ${theme.colors.white};
+  }
+  &.button_login_kakao {
+    color: ${theme.colors.black};
   }
 `;
