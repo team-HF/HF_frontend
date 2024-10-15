8c113b17bb6df43000c5316a9e45cb99cc744934
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2024-10-16 01:59:10 +0900

    fix: google oauth 에러 수정

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,18 +1,18 @@
 import * as S from "./style";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import kakaoOauth from "./api/kakaoOauth";
 import googleOauth from "./api/googleOauth";
-import { useParams } from "react-router-dom";
 
-const KAKAO_BASE_URL = import.meta.env.VITE_KAKAO_BASE_URL;
+const KAKAO_OAUTH_URL = import.meta.env.VITE_KAKAO_OAUTH_URL;
 const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
 const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-const KAKAO_AUTH_URL = `${KAKAO_BASE_URL}/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code&prompt=login`;
+const KAKAO_AUTHORIZATION_CODE_URL = `${KAKAO_OAUTH_URL}/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code&prompt=login`;
 
 const GOOGLE_OAUTH_URL = import.meta.env.VITE_GOOGLE_OAUTH_URL;
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 const GOOGLE_REDIRECT_URI = import.meta.env.VITE_GOOGLE_REDIRECT_URI;
-const GOOGLE_AUTH_URL = `${GOOGLE_OAUTH_URL}&redirect_uri=${GOOGLE_REDIRECT_URI}&client_id=${GOOGLE_CLIENT_ID}`;
+const GOOGLE_AUTHORIZATION_CODE_URL = `${GOOGLE_OAUTH_URL}&redirect_uri=${GOOGLE_REDIRECT_URI}&client_id=${GOOGLE_CLIENT_ID}`;
 
 const Login = () => {
   const { oauth } = useParams();
@@ -25,14 +25,14 @@ const Login = () => {
         kakaoOauth(code);
       }
     }
-  }, []);
+  }, [code, oauth]);
   return (
     <S.Container>
       <S.BtnBox>
         <S.OauthBtn
           className="button_login_google"
           onClick={() => {
-            window.location.href = GOOGLE_AUTH_URL;
+            window.location.href = GOOGLE_AUTHORIZATION_CODE_URL;
           }}
         >
           <S.LogoIcon src="/oauth/google.png" />
@@ -43,7 +43,7 @@ const Login = () => {
         <S.OauthBtn
           className="button_login_kakao"
           onClick={() => {
-            window.location.href = KAKAO_AUTH_URL;
+            window.location.href = KAKAO_AUTHORIZATION_CODE_URL;
           }}
         >
           <S.LogoIcon src="/oauth/kakao.png" className="kakao" />
