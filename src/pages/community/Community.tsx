541a26a4c5f16ac9062275d5c95286a6b568c0a8
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2024-10-17 00:15:37 +0900

    feat: community 무한스크롤 추가

--- src/pages/community/Community.tsx
+++ src/pages/community/Community.tsx
@@ -1,42 +1,23 @@
 import * as S from "./style";
-import { useEffect, useState } from "react";
+import { Virtuoso } from "react-virtuoso";
+import { useAxios } from "../../shared/utils/useAxios";
 import { ContentsTypeData } from "../../entities/community/contents-type-data";
+import { useInfiniteScroll } from "../../shared/utils/useInfiniteScroll";
+import { useCallback, useEffect, useState } from "react";
 import FilterBar from "../../widgets/community/filter-bar/FilterBar";
 import ContentsType from "../../widgets/community/contents-type/ContentsType";
-import CommunityHeader from "../../widgets/community/community-header/CommunityHeader";
-import PostPreviewList from "../../shared/ui/post-preview-list/PostPreviewList";
 import FloatingButton from "../../widgets/community/floating-button/FloatingButton";
-import getPostList from "./api/useGetPostList";
-import { useAxios } from "../../shared/utils/useAxios";
-
-interface postData {
-  postId: number;
-  category: string;
-  title: string;
-  content_part: string;
-  creation_time: string;
-  view_count: number;
-  like_count: number;
-  comment_count: number;
-  fitnessLevel: string;
-}
-
-const data = Array.from({ length: 100 }, () => ({
-  postId: 0,
-  category: "헬스장 추천",
-  title: "헬스장 추천",
-  content: "헬스장 추천 내용",
-  creationTime: "2024-10-15T17:12:08.826Z",
-  viewCount: 0,
-  likeCount: 0,
-  commentCount: 0,
-  fitnessLevel: "고수",
-}));
+import CommunityHeader from "../../widgets/community/community-header/CommunityHeader";
+import PostPreviewList, {
+  PostData,
+} from "../../shared/ui/post-preview-list/PostPreviewList";
 
 const Community = () => {
   const { axiosInstance } = useAxios();
-  const [currentPage, setCurrentPage] = useState<number>(1); //수정필요
-  const [postList, setPostList] = useState<postData[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [postList, setPostList] = useState<PostData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState<boolean>(true);
   const [contentType, setContentType] = useState<string>(
     ContentsTypeData[0].name
   );
@@ -46,23 +27,50 @@ const Community = () => {
     );
     return findObject ? findObject.filterType : "dropdown";
   };
-  const contentsList = data.map((data, idx) => (
-    <PostPreviewList key={`community_post_${idx}`} data={data} />
-  ));
-  useEffect(() => {
-    (async () => {
-      const response = await getPostList(axiosInstance, currentPage);
-      if (Array.isArray(response)) {
-        setPostList(response);
+  const getPosts = useCallback(async () => {
+    if (!hasMore || isLoading) return;
+    setIsLoading(true);
+    try {
+      const response = await axiosInstance.get("/hf/list", {
+        params: {
+          page: currentPage,
+        },
+      });
+      const newPostList = response.data.content;
+      if (!Array.isArray(newPostList)) {
+        throw new Error("Error fetching post list.");
       }
-    })();
-  }, [currentPage]);
+      const totalPages: number = response.data.totalPages;
+      setPostList([...postList, ...newPostList]);
+      setHasMore(currentPage < totalPages);
+      setCurrentPage(currentPage + 1);
+    } catch (error) {
+      console.error("Error fetching post list:", error);
+      setHasMore(false);
+      throw error;
+    } finally {
+      setIsLoading(false);
+    }
+  }, [currentPage, hasMore, isLoading]);
+  const { virtuosoRef } = useInfiniteScroll(getPosts, hasMore, isLoading);
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <S.Container>
       <CommunityHeader />
       <ContentsType contentType={contentType} setContentType={setContentType} />
       <FilterBar filterType={getFilterType()} />
-      <S.PostContainer>{contentsList}</S.PostContainer>
+      <S.PostContainer>
+        <Virtuoso
+          useWindowScroll
+          ref={virtuosoRef}
+          data={postList}
+          endReached={getPosts}
+          itemContent={(_, post: PostData) => <PostPreviewList {...post} />}
+        />
+        {isLoading && <p>로딩중...</p>}
+      </S.PostContainer>
       <FloatingButton />
     </S.Container>
   );
