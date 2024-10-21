7144e586e64ebc51eb1f1ad485961f52fdbbac92
Namhyun <jenple9709@gmail.com>
2024-10-21 15:51:25 +0900

    design: 매칭 진행중, 종료 ui

---
+++ src/widgets/chat-lobby/card/FinshedMatchCard.tsx
@@ -0,0 +1,12 @@
+import Label from '../../../shared/ui/label/Label';
+
+export default function FinishedMatchCard() {
+  return (
+    <Label
+      text="종료"
+      backgroundColor="##F8F8F8"
+      fontColor="#0B2D48"
+      borderColor="#F8F8F8"
+    />
+  );
+}
