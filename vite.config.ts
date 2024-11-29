1394b9652871d389c25c095c87ec3caf9bb4f928
Namhyun <jenple9709@gmail.com>
2024-11-29 13:20:25 +0900

    fix: vite 중복 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000, // Change the port to 3000
-  },
   plugins: [react()],
   server: {
     port: 3000,
