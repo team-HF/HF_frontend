f8f03257784fa1b191112b5b18519d34bc12a3b4
Namhyun <jenple9709@gmail.com>
2024-12-01 23:52:22 +0900

    design: ProfileBox 컴포넌트 디자인 수정

--- src/entities/my-page/ui/ProfileBox.tsx
+++ src/entities/my-page/ui/ProfileBox.tsx
@@ -1,7 +1,22 @@
 import Hashtag from '../../../shared/ui/hashtag/Hashtag';
 import * as S from './profile-box.style';
+import { useGetMyData } from '../../../shared/api/useGetMyData';
 
 export default function ProfileBox() {
+  const { data: myData, isLoading, isError } = useGetMyData();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error</p>;
+
+  const nickname = myData?.nickname || '닉네임 정보를 불러올 수 없습니다.';
+  const introduction = myData?.introduction || '자기소개를 불러올 수 없습니다.';
+  const hashtags = [
+    myData?.companionStyle,
+    myData?.fitnessEagerness,
+    myData?.fitnessKind,
+    myData?.fitnessObjective,
+  ];
+
   return (
     <S.Container>
       <S.ProfileContainer>
@@ -12,14 +27,13 @@ export default function ProfileBox() {
           />
         </S.ProfileIconContainer>
         <S.ProfileTextContainer>
-          <S.ProfileName>유저 1</S.ProfileName>
+          <S.ProfileName>{nickname}</S.ProfileName>
           <S.ProfileHashtagContainer>
-            <Hashtag text="#소규모형" />
-            <Hashtag text="#귀차니즘형" />
-            <Hashtag text="#기능성피트니스위주" />
-            <Hashtag text="#헬스헬스고강도헬스" />
+            {hashtags.map((tag, index) => (
+              <Hashtag key={index} text={`#${tag}`} />
+            ))}
           </S.ProfileHashtagContainer>
-          <S.ProfileIntroduction>한줄 소개</S.ProfileIntroduction>
+          <S.ProfileIntroduction>{introduction}</S.ProfileIntroduction>
         </S.ProfileTextContainer>
       </S.ProfileContainer>
     </S.Container>
