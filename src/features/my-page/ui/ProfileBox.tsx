f5da8f59dadfba27fd66beccec32c8223d23641c
Namhyun <jenple9709@gmail.com>
2024-12-11 23:57:10 +0900

    design: my-page profile부분 디자인 변경

--- src/features/my-page/ui/ProfileBox.tsx
+++ src/features/my-page/ui/ProfileBox.tsx
@@ -1,17 +1,31 @@
 import Hashtag from '../../../shared/ui/hashtag/Hashtag';
 import * as S from './profile-box.style';
+import {
+  COMPANION_STYLE_MAP,
+  FITNESS_EAGERNESS_MAP,
+  FITNESS_KIND_MAP,
+  FITNESS_LEVEL_MAP,
+  FITNESS_OBJECTIVE_MAP,
+  getCompanionStyleText,
+  getFitnessEagernessText,
+  getFitnessKindText,
+} from '../../../shared/constants/fitness-category';
 
 type ProfileBoxProps = {
   myData?: {
     nickname: string;
     introduction: string;
-    companionStyle: string;
-    fitnessEagerness: string;
-    fitnessKind: string;
-    fitnessObjective: string;
+    tier: {
+      fitnessLevel: string;
+      tier: number;
+    };
+    companionStyle: keyof typeof COMPANION_STYLE_MAP;
+    fitnessEagerness: keyof typeof FITNESS_EAGERNESS_MAP;
+    fitnessKind: keyof typeof FITNESS_KIND_MAP;
+    fitnessObjective: keyof typeof FITNESS_OBJECTIVE_MAP;
+    fitnessLevel: keyof typeof FITNESS_LEVEL_MAP;
   };
 };
-
 export default function ProfileBox({ myData }: ProfileBoxProps) {
   if (!myData) {
     return (
@@ -25,19 +39,24 @@ export default function ProfileBox({ myData }: ProfileBoxProps) {
 
   const {
     nickname,
+    tier,
     introduction,
     companionStyle,
     fitnessEagerness,
     fitnessKind,
     fitnessObjective,
+    fitnessLevel,
   } = myData;
   const hashtags = [
-    companionStyle,
-    fitnessEagerness,
-    fitnessKind,
-    fitnessObjective,
-  ].filter(Boolean);
-
+    getCompanionStyleText(companionStyle as keyof typeof COMPANION_STYLE_MAP),
+    getFitnessEagernessText(
+      fitnessEagerness as keyof typeof FITNESS_EAGERNESS_MAP
+    ),
+    getFitnessKindText(fitnessKind as keyof typeof FITNESS_KIND_MAP),
+    FITNESS_OBJECTIVE_MAP[
+      fitnessObjective as keyof typeof FITNESS_OBJECTIVE_MAP
+    ],
+  ];
   return (
     <S.Container>
       <S.ProfileContainer>
@@ -48,7 +67,12 @@ export default function ProfileBox({ myData }: ProfileBoxProps) {
           />
         </S.ProfileIconContainer>
         <S.ProfileTextContainer>
-          <S.ProfileName>{nickname}</S.ProfileName>
+          <S.NameWrapper>
+            <S.ProfileName>{nickname}</S.ProfileName>
+            <S.LevelWrapper $fitnessLevel={fitnessLevel}>
+              {`${FITNESS_LEVEL_MAP[fitnessLevel]} LV.${tier.tier}`}
+            </S.LevelWrapper>
+          </S.NameWrapper>
           <S.ProfileHashtagContainer>
             {hashtags.map((tag, index) => (
               <Hashtag key={index} text={`#${tag}`} />
