7d91f2fc02134c70c6af56cfa5246a81a6f80769
Namhyun <jenple9709@gmail.com>
2024-12-18 23:57:26 +0900

    refactor: profile-setting 스토어 변경

--- src/features/profile-setting/store/profile-setting-store.ts
+++ src/features/profile-setting/store/profile-setting-store.ts
@@ -4,16 +4,21 @@ import { persist } from 'zustand/middleware';
 type ProfileSettingStore = {
   image: File | null;
   setImage: (option: File) => void;
+
   introduction: string | null;
   setIntroduction: (option: string) => void;
   nickname: string | null;
   setNickname: (option: string) => void;
-  birth: string | null;
-  setBirth: (option: string) => void;
-  gender: string | null;
-  setGender: (option: string) => void;
+
   specs: string | null;
   setSpecs: (option: string) => void;
+
+  cd1: string | null;
+  setCd1: (option: string) => void;
+  cd2: string | null;
+  setCd2: (option: string) => void;
+  cd3: string | null;
+  setCd3: (option: string) => void;
 };
 
 export const useProfileSettingStore = create(
@@ -21,16 +26,22 @@ export const useProfileSettingStore = create(
     (set) => ({
       image: null,
       setImage: (option) => set({ image: option }),
+
       introduction: null,
       setIntroduction: (option) => set({ introduction: option }),
       nickname: null,
       setNickname: (option) => set({ nickname: option }),
-      birth: null,
-      setBirth: (option) => set({ birth: option }),
-      gender: null,
-      setGender: (option) => set({ gender: option }),
+
       specs: null,
       setSpecs: (option) => set({ specs: option }),
+      cd1: null,
+      setCd1: (option) => set({ cd1: option }),
+
+      cd2: null,
+      setCd2: (option) => set({ cd2: option }),
+
+      cd3: null,
+      setCd3: (option) => set({ cd3: option }),
     }),
 
     {
