d759b2bbd975554170b9f8dc917f5f5103b1a944
Namhyun <jenple9709@gmail.com>
2025-01-18 17:36:44 +0900

    design: 닉네임 중복 검사 디자인

---
+++ src/features/profile-setting/ui/DuplicateNicknameButton.tsx
@@ -0,0 +1,9 @@
+import * as S from './duplicate-nickname-button-style';
+
+export default function DuplicateNicknameButton() {
+  return (
+    <S.Container>
+      <S.StyledSpan>중복검사</S.StyledSpan>
+    </S.Container>
+  );
+}
