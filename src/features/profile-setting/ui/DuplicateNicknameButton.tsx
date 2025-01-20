c1d713326da30a65aba508f5a32b55a3214f42c6
Namhyun <jenple9709@gmail.com>
2025-01-20 21:59:47 +0900

    feat: 닉네임 중복 검사 기능 추가

--- src/features/profile-setting/ui/DuplicateNicknameButton.tsx
+++ src/features/profile-setting/ui/DuplicateNicknameButton.tsx
@@ -1,9 +1,31 @@
 import * as S from './duplicate-nickname-button-style';
+import { useGetDuplicateNickname } from '../api/useGetDuplicateNickName';
 
-export default function DuplicateNicknameButton() {
+interface DuplicateNicknameButtonProps {
+  nickname: string | null;
+  onSuccess: (validatedName: string) => void;
+}
+
+export default function DuplicateNicknameButton({
+  nickname,
+  onSuccess,
+}: DuplicateNicknameButtonProps) {
+  const { refetch } = useGetDuplicateNickname(nickname);
+  const onClick = async () => {
+    const result = await refetch();
+    if (result.data?.content === true) {
+      alert('이미 사용중인 닉네임입니다.');
+    } else {
+      alert('사용 가능한 닉네임입니다.');
+      if (nickname) {
+        onSuccess(nickname);
+      }
+    }
+  };
+
   return (
     <S.Container>
-      <S.StyledSpan>중복검사</S.StyledSpan>
+      <S.StyledSpan onClick={onClick}>중복검사</S.StyledSpan>
     </S.Container>
   );
 }
