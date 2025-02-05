98ea6c16912b370839da444b2f0129050e928381
Namhyun <jenple9709@gmail.com>
2025-02-05 17:26:01 +0900

    refactor: Matching 페이지 데이터 임시 데이터 => 상대 데이터 값 변경

--- src/entities/matching/ui/PartnerInfo.tsx
+++ src/entities/matching/ui/PartnerInfo.tsx
@@ -1,22 +1,30 @@
-import { useGetMyData } from '../../../shared/api/useGetMyData';
 import LevelLabel from '../../../shared/ui/level-label/LevelLabel';
 import * as S from './partner-info-style.ts';
+import { useGetMatchingUserInfo } from '../../../features/matching/api/useGetMatchingUserInfo.ts';
 
-export default function PartnerInfo() {
-  //매칭 상대 데이터로 변경해야 함
-
-  const { data: 임시데이터, isLoading } = useGetMyData();
-  if (isLoading || !임시데이터) {
+export default function PartnerInfo({
+  matchingUserId,
+}: {
+  matchingUserId: number;
+}) {
+  const {
+    data: matchingUserData,
+    isLoading,
+    error,
+  } = useGetMatchingUserInfo(matchingUserId);
+  console.log(matchingUserData);
+  if (isLoading || !matchingUserData) {
     return <div>로딩</div>;
   }
-  const location = `${임시데이터!.cd1 + 임시데이터!.cd2}`;
+  if (error) return <div>error.</div>;
+  const location = `${matchingUserData!.cd1 + matchingUserData!.cd2}`;
 
   return (
     <S.Container>
-      <S.ImageWrapper $profileImageUrl={임시데이터?.profileImageUrl} />
+      <S.ImageWrapper $profileImageUrl={matchingUserData?.profileImageUrl} />
       <S.RightWrapper>
         <S.NameAndLevelWrapper>
-          <S.StyleName>{임시데이터?.nickname}</S.StyleName>
+          <S.StyleName>{matchingUserData?.nickname}</S.StyleName>
           <LevelLabel />
         </S.NameAndLevelWrapper>
         <S.LocationWrapper>
--- src/pages/matching/Matching.tsx
+++ src/pages/matching/Matching.tsx
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import PartnerInfo from '../../entities/matching/ui/PartnerInfo';
 import MatchingButton from '../../features/matching/ui/MatchingButton';
 import ScheduleForm from '../../features/matching/ui/ScheduleForm';
@@ -5,10 +6,12 @@ import BackHeader from '../../shared/ui/back-header/BackHeader';
 import * as S from './style';
 
 export default function Matching() {
+  const { id } = useParams();
+  const matchingUserId = parseInt(id!, 10);
   return (
     <S.Container>
       <BackHeader text="매칭 신청" style={{ padding: '0px' }} />
-      <PartnerInfo />
+      <PartnerInfo matchingUserId={matchingUserId} />
       <S.DetailDiv>상세 일정</S.DetailDiv>
       <ScheduleForm />
       <S.BottomWrapper>
