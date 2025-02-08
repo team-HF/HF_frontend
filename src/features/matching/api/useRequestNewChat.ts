67fb835652d3a543ef2b3a9324401b67cd1c7739
Namhyun <jenple9709@gmail.com>
2025-02-08 19:59:41 +0900

    feat: 채팅방 리스트 가져오는 기능 추가

---
+++ src/features/matching/api/useRequestNewChat.ts
@@ -0,0 +1,35 @@
+// src/features/chat/useRequestChat.ts
+import { useContext, useCallback } from 'react';
+import { SocketContext } from '../../../app/providers/SocketProvider';
+
+interface ChatParticipationRequestDto {
+  requesterId: number;
+  chatTargetId: number;
+}
+
+export function useRequestChat() {
+  const socketContext = useContext(SocketContext);
+
+  if (!socketContext) {
+    throw new Error('소켓에 연결되어 있지 않습니다.');
+  }
+
+  const { stompClient, isConnected } = socketContext;
+
+  // 채팅 신청을 STOMP로 전송하는 함수
+  const requestChat = useCallback(
+    (payload: ChatParticipationRequestDto) => {
+      if (!stompClient) {
+        console.warn('STOMP client not ready');
+        return;
+      }
+      stompClient.publish({
+        destination: '/hf/app/chat/request',
+        body: JSON.stringify(payload),
+      });
+    },
+    [stompClient]
+  );
+
+  return { requestChat, isConnected };
+}
