538854bccf3428e7c1942119a217726c5d7ebbef
Namhyun <jenple9709@gmail.com>
2025-02-19 19:33:10 +0900

    feat: 메뉴 기능 추가

--- src/features/chat/ui/ChatHeader.tsx
+++ src/features/chat/ui/ChatHeader.tsx
@@ -2,12 +2,22 @@ import * as S from './chat-header-style.ts';
 import { useGetMyData } from '../../../shared/api/useGetMyData.ts';
 import LevelLabel from '../../../shared/ui/level-label/LevelLabel.tsx';
 import { useNavigate } from 'react-router-dom';
-import OnGoingMatchCard from '../../chat-lobby/ui/card/ui/OnGoingMatchCard.tsx';
+import RequestMatchingLabel from './RequestMatchingLabel.tsx';
+import { useState } from 'react';
+import ChatMenuModal from './ChatMenuModal.tsx';
 
-export default function ChatHeader() {
+type ChatHeaderProps = {
+  chatroomId: string;
+};
+
+export default function ChatHeader({ chatroomId }: ChatHeaderProps) {
   const navigate = useNavigate();
   const onBackCluck = () => {
     navigate(-1);
+  };
+  const [isOpen, setIsOpen] = useState(false);
+  const onClick = () => {
+    setIsOpen(true);
   };
   const { data: myData } = useGetMyData();
   return (
@@ -24,9 +34,14 @@ export default function ChatHeader() {
         <LevelLabel />
       </S.CenterWrapper>
       <S.RightWrapper>
-        <OnGoingMatchCard />
-        <S.StyleMenu src="/svg/menu-dot-col-icon.svg" alt="menu-icon" />
+        <RequestMatchingLabel chatroomId={chatroomId} />
+        <S.StyleMenu
+          src="/svg/menu-dot-col-icon.svg"
+          alt="menu-icon"
+          onClick={onClick}
+        />
       </S.RightWrapper>
+      {isOpen && <ChatMenuModal onClose={() => setIsOpen(false)} />}
     </S.Container>
   );
 }
