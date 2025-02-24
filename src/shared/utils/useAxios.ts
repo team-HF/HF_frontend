dfcb87b021a1d4ea94ba1599729ba3bc7430d2b9
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2025-02-24 19:32:53 +0900

    feat: access token 재발급

--- src/shared/utils/useAxios.ts
+++ src/shared/utils/useAxios.ts
@@ -25,25 +25,21 @@ export const useAxios = () => {
   let isRefreshing = false;
   let refreshSubscribers: ((token: string) => void)[] = [];
 
-  // 🔹 토큰 갱신을 기다리는 요청을 처리하는 함수
   const onTokenRefreshed = (newToken: string) => {
     refreshSubscribers.forEach((callback) => callback(newToken));
     refreshSubscribers = [];
   };
 
-  // 🔹 403 또는 401 발생 시, 자동으로 access_token 재발급
   axiosInstance.interceptors.response.use(
     (response) => response, // 성공 응답 그대로 반환
     async (error) => {
       const originalRequest = error.config;
 
-      // access_token 만료(403 또는 401)이고, 재요청이 아니라면
       if (
         (error.response?.status === 403 || error.response?.status === 401) &&
         !originalRequest._retry
       ) {
         if (isRefreshing) {
-          // 이미 refresh 요청이 진행 중이면, 새 요청을 대기 상태로 둠
           return new Promise((resolve) => {
             refreshSubscribers.push((token) => {
               originalRequest.headers.Authorization = `Bearer ${token}`;
@@ -56,7 +52,6 @@ export const useAxios = () => {
         isRefreshing = true;
 
         try {
-          // 🔹 Refresh Token을 사용해 새로운 Access Token 요청
           const response = await axios.get(
             `${import.meta.env.VITE_BASE_URL}/oauth/token/refresh`,
             {
@@ -64,15 +59,12 @@ export const useAxios = () => {
             }
           );
           const newAccessToken = response.data.content.accessToken;
-          console.log(newAccessToken);
           Cookies.set("access_token", newAccessToken, {
             secure: true,
             sameSite: "None",
           });
-          // 대기 중인 요청들을 새 토큰으로 재시도
           onTokenRefreshed(newAccessToken);
 
-          // 원래 요청을 다시 실행
           originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
           return axiosInstance(originalRequest);
         } catch (refreshError) {
