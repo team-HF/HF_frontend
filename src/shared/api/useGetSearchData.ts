000c45da853fb2c1881f22173e49346ffe844593
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2025-02-27 01:10:24 +0900

    fix: 403에러 수정

--- src/shared/api/useGetSearchData.ts
+++ src/shared/api/useGetSearchData.ts
@@ -2,13 +2,13 @@ import { useAxios as Axios } from "../utils/useAxios";
 
 export const useGetSearchData = async () => {
   const { axiosInstance } = Axios();
-
   try {
+    const urlParams = new URLSearchParams(window.location.search);
+    const queryParams = Object.fromEntries(urlParams.entries());
     const response = await axiosInstance.get("/hf/search", {
       params: {
         size: 5,
-        companionStyles: "SMALL",
-        keyword: "안녕",
+        ...queryParams,
       },
     });
     return response.data.content;
--- src/widgets/post-detail/comment-card/api/useUpdateComment.ts
+++ src/widgets/post-detail/comment-card/api/useUpdateComment.ts
@@ -1,12 +1,19 @@
 import axios from "axios";
 import { useAxios as Axios } from "../../../../shared/utils/useAxios";
+import Cookies from "js-cookie";
 
 export const useUpdateComment = async (commentId: number, content: string) => {
   const { axiosInstance } = Axios();
+  const accessToken = Cookies.get("access_token");
+  console.log(accessToken);
   try {
-    const response = await axiosInstance.patch(`/hf/comments/${commentId}`, {
-      content,
-    });
+    const response = await axiosInstance.patch(
+      `/hf/comments/${commentId}`,
+      {
+        content,
+      },
+      { headers: { Authorization: `Bearer ${accessToken}` } }
+    );
 
     console.log("수정 성공:", response.data);
     return response.data;
