46d8258de502f0b50a7f682e30c7eb1bb5fc3a05
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2025-03-06 00:57:34 +0900

    chore: 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -47,9 +47,7 @@ function App() {
         ) {
           const eventData = JSON.parse(event.data);
           addNotification(eventData);
-          console.log("📩 Alarm received:", eventData);
         } else {
-          console.log(event);
           return;
         }
       } catch (error) {
@@ -125,92 +123,6 @@ function App() {
       )}
     </ThemeProvider>
   );
-  // return (
-  //   <ThemeProvider theme={theme}>
-  //     <Routes>
-  //       <Route path="/my-page" element={<MyPage />} />
-  //       <Route path="/register/exercise-style" element={<ExerciseOption />} />
-  //       <Route path="/register/profile" element={<Profile />} />
-
-  //       <Route path="/profile-setting">
-  //         <Route index element={<ProfileSetting />} />
-  //         <Route path="exercise-style" element={<ExerciseStyle />} />
-  //         <Route path="introduction" element={<Introduction />} />
-  //       </Route>
-
-  //       <Route path="/chat-lobby" element={<ChatLobby />} />
-  //       <Route path="/chat/:chatRoomId" element={<Chat />} />
-  //       <Route path="community" element={<Community />} />
-  //       <Route path="community/post-register" element={<PostRegister />} />
-  //       <Route path="community/post-update/:id" element={<PostRegister />} />
-  //       <Route path="community/post-detail/:id" element={<PostDetail />} />
-  //       <Route path="/matching/:id" element={<Matching />} />
-  //       <Route path="/matching-review" element={<MatchingReview />} />
-  //       <Route path="member/:id/profile" element={<UserProfile />} />
-  //       <Route path="/" element={<ProfileSearch />} />
-  //       <Route path="/search-result" element={<SearchResult />} />
-  //       <Route path="/agreement" element={<Agreement />} />
-  //       <Route path="/login" element={<Login />} />
-  //     </Routes>
-  //   </ThemeProvider>
-  // );
 }
-
-// export default App;
-
-// import { Routes, Route, useNavigate } from 'react-router-dom';
-// import { theme } from './app/theme';
-// import { ThemeProvider } from 'styled-components';
-// import MyPage from './pages/my-page/MyPage';
-// import ExerciseOption from './pages/exercise-option/ExerciseOption';
-// import ProfileSetting from './pages/profile-setting/ProfileSetting';
-// import Profile from './pages/profile/Profile';
-// import Login from './pages/login/Login';
-// import Introduction from './pages/introduction/Introduction';
-// import Community from './pages/community/Community';
-// import ChatLobby from './pages/chat-lobby/ChatLobby';
-// import PostRegister from './pages/post-register/PostRegister';
-// import PostDetail from './pages/post-detail/PostDetail';
-// import ExerciseStyle from './pages/exercise-style/ExerciseStyle';
-// import Matching from './pages/matching/Matching';
-// import MatchingReview from './pages/matching-review/MatchingReview';
-// import UserProfile from './pages/user-profile/UserProfile';
-// import ProfileSearch from './pages/profile-serch/ProfileSearch';
-// import { Chat } from './pages/chat/Chat';
-// import { SocketProvider } from './app/providers/SocketProvider';
-// import { SubscriptionProvider } from './app/providers/SubscriptionProvider';
-// import { useGetMyData } from './shared/api/useGetMyData';
-// import SearchResult from './pages/search-result/SearchResult';
-
-// function App() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <Routes>
-//         <Route path="/my-page" element={<MyPage />} />
-//         <Route path="/register/exercise-style" element={<ExerciseOption />} />
-//         <Route path="/register/profile" element={<Profile />} />
-
-//         <Route path="/profile-setting">
-//           <Route index element={<ProfileSetting />} />
-//           <Route path="exercise-style" element={<ExerciseStyle />} />
-//           <Route path="introduction" element={<Introduction />} />
-//         </Route>
-
-//         <Route path="/chat-lobby" element={<ChatLobby />} />
-//         <Route path="/chat/:chatRoomId" element={<Chat />} />
-//         <Route path="community" element={<Community />} />
-//         <Route path="community/post-register" element={<PostRegister />} />
-//         <Route path="community/post-update/:id" element={<PostRegister />} />
-//         <Route path="community/post-detail/:id" element={<PostDetail />} />
-//         <Route path="/matching/:id" element={<Matching />} />
-//         <Route path="/matching-review" element={<MatchingReview />} />
-//         <Route path="member/:id/profile" element={<UserProfile />} />
-//         <Route path="/" element={<ProfileSearch />} />
-//         <Route path="/search-result" element={<SearchResult />} />
-//       <Route path="/login" element={<Login />} />
-//       </Routes>
-//     </ThemeProvider>
-//   );
-// }
 
 export default App;
