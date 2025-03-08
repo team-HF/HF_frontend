417e3ffe944b31fefd893c04b0846e91da217c2b
오승찬 <oseungchan@oseungchan-ui-MacBookAir.local>
2025-03-08 10:27:33 +0900

    chore: 코드 정리

--- src/pages/user-profile/UserProfile.tsx
+++ src/pages/user-profile/UserProfile.tsx
@@ -1,15 +1,15 @@
-import * as S from './style';
-import LogoHeader from '../../shared/ui/logo-header/Header';
-import Career from '../../widgets/user-profile/career/Career';
-import PageForm from '../../shared/ui/page-form/PageForm';
-import UserDataDefault from '../../widgets/user-profile/user-data-default/UserDataDefault';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useUserDetailStore } from '../profile/store/user-detail-store';
-import { useUserProfileStore } from '../../shared/store/user-profile-store';
-import { useGetUserData as getUserData } from '../../shared/api/useGetUserData';
-import { useGetUserDetail as getUserDetail } from './api/useGetUserDetail';
-import Review from '../../widgets/user-profile/review/Review';
+import * as S from "./style";
+import LogoHeader from "../../shared/ui/logo-header/Header";
+import Career from "../../widgets/user-profile/career/Career";
+import PageForm from "../../shared/ui/page-form/PageForm";
+import UserDataDefault from "../../widgets/user-profile/user-data-default/UserDataDefault";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useUserDetailStore } from "../profile/store/user-detail-store";
+import { useUserProfileStore } from "../../shared/store/user-profile-store";
+import { useGetUserData as getUserData } from "../../shared/api/useGetUserData";
+import { useGetUserDetail as getUserDetail } from "./api/useGetUserDetail";
+import Review from "../../widgets/user-profile/review/Review";
 
 const UserProfile = () => {
   const { id } = useParams();
