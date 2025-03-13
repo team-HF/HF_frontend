731040c27be2aeb55b25bcd6f8034f335bbb230a
Namhyun <jenple9709@gmail.com>
2025-03-13 21:02:50 +0900

    design: 채팅룸 메뉴 헤더 매칭중단 버튼 추가

--- src/features/chat/ui/ChatMenuModal.tsx
+++ src/features/chat/ui/ChatMenuModal.tsx
@@ -1,11 +1,32 @@
+import { useNavigate } from 'react-router-dom';
 import * as S from './chat-menu-modal.style';
+import { useState } from 'react';
+import ChatConfirmModal from '../../../shared/ui/chat-confirm-modal/ChatConfirmModal';
+import StopMatchingRoom from './StopMatchingRoom';
 
 type ChatMenuModal = {
   onClose: () => void;
+  matchingUserId: number;
 };
-export default function ChatMenuModal({ onClose }: ChatMenuModal) {
+export default function ChatMenuModal({
+  onClose,
+  matchingUserId,
+}: ChatMenuModal) {
+  const navigate = useNavigate();
+  const [isOpenExitModal, setIsOpenExitModal] = useState(false);
+  const [isOpenStopModal, setIsOpenStopModal] = useState(false);
+
+  const exitRoom = () => {
+    setIsOpenExitModal(true);
+  };
+  const stopMatching = () => {
+    setIsOpenStopModal(true);
+  };
   const modalClose = () => {
     onClose();
+  };
+  const navigateToProfile = () => {
+    navigate(`/member/${matchingUserId}/profile`);
   };
   return (
     <div>
@@ -18,10 +39,17 @@ export default function ChatMenuModal({ onClose }: ChatMenuModal) {
                 alt="profile-icon"
                 style={{ marginLeft: '36px' }}
               />
-              <S.ModalText>프로필 조회하기</S.ModalText>
+              <S.ModalText onClick={navigateToProfile}>
+                프로필 조회하기
+              </S.ModalText>
             </S.ProfileButton>
 
             <S.ButtonWrapper>
+              <S.StyledButton onClick={stopMatching}>
+                <S.ModalSvg src="/svg/ban-icon.svg" alt="ban-icon" />
+                <S.ModalText>채팅중단</S.ModalText>
+              </S.StyledButton>
+
               <S.StyledButton>
                 <S.ModalSvg src="/svg/ban-icon.svg" alt="ban-icon" />
                 <S.ModalText>차단하기</S.ModalText>
@@ -40,7 +68,7 @@ export default function ChatMenuModal({ onClose }: ChatMenuModal) {
                 <S.ModalText>알림 끄기</S.ModalText>
               </S.StyledButton>
 
-              <S.StyledButton>
+              <S.StyledButton onClick={exitRoom}>
                 <S.ModalSvg src="/svg/exit-icon.svg" alt="exit" />
                 <S.ModalText style={{ color: 'red' }}>
                   채팅방 나가기
@@ -52,6 +80,24 @@ export default function ChatMenuModal({ onClose }: ChatMenuModal) {
             <S.CancelButton onClick={modalClose}>취소</S.CancelButton>
           </S.CancelButtonWrapper>
         </S.Modal>
+        {isOpenExitModal && (
+          <>
+            <S.ModalOverlay />
+            <ChatConfirmModal
+              onConfirm={() => setIsOpenExitModal(false)}
+              onCancel={() => setIsOpenExitModal(false)}
+            />
+          </>
+        )}
+        {isOpenStopModal && (
+          <>
+            <S.ModalOverlay />
+            <StopMatchingRoom
+              onConfirm={() => setIsOpenStopModal(false)}
+              onCancel={() => setIsOpenStopModal(false)}
+            />
+          </>
+        )}
       </S.Backdrop>
     </div>
   );
