7db66700ae6d0aab07421346916b3cd734d397b3
Namhyun <jenple9709@gmail.com>
2025-03-17 20:31:32 +0900

    refactor: useDeleteWish 함수 tanstack-query를 사용하여 수정

--- src/shared/api/useDeleteWish.ts
+++ src/shared/api/useDeleteWish.ts
@@ -1,19 +1,24 @@
-import axiosInstance from "../utils/useAxios";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import axiosInstance from '../utils/useAxios';
 
 interface PostWishProps {
   wisherId: number | undefined;
   wishedId: number | undefined;
 }
 
-export const useDeleteWish = async ({ wisherId, wishedId }: PostWishProps) => {
-  const data = {
-    wisherId: wisherId,
-    wishedId: wishedId,
-  };
-  try {
-    const response = await axiosInstance.delete("/hf/wish", { data });
-    console.log("삭제 성공:", response.data);
-  } catch (error) {
-    console.error("삭제 실패:", error);
-  }
+const deleteWish = async ({ wisherId, wishedId }: PostWishProps) => {
+  const data = { wisherId, wishedId };
+  const response = await axiosInstance.delete('/hf/wish', { data });
+  return response.data;
+};
+
+export const useDeleteWish = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteWish,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['myWishList'] });
+    },
+  });
 };
