e08fceba9a636b09e25403ea5685220669f79311
Namhyun <jenple9709@gmail.com>
2025-03-26 20:14:19 +0900

    feat: 매칭 요청 메시지 추가

--- src/features/chat/ui/ChatRequestBubble.tsx
+++ src/features/chat/ui/ChatRequestBubble.tsx
@@ -1,35 +1,72 @@
-import React from 'react';
+import BubbleMessageButton from './BubbleMessageButton';
 import * as S from './chat-request-bubble.style';
 
-const MatchingBubble: React.FC = () => {
-  return (
-    <S.Container>
-      <S.Card>
-        <S.HeaderWrapper>
-          <S.Header>매칭 완료</S.Header>
-        </S.HeaderWrapper>
-        <S.Content>
-          <S.Row>
-            <S.Label>날짜:</S.Label>
-            <S.Value>12월 16일</S.Value>
-          </S.Row>
-          <S.Row>
-            <S.Label>시간:</S.Label>
-            <S.Value>PM 7:35</S.Value>
-          </S.Row>
-          <S.Row>
-            <S.Label>장소:</S.Label>
-            <S.Value>송파구 퍼스트휘트니스...</S.Value>
-          </S.Row>
-        </S.Content>
-        <S.ButtonWrapper>
-          <S.Button onClick={() => console.log('매칭 조회 clicked')}>
-            매칭 조회
-          </S.Button>
-        </S.ButtonWrapper>
-      </S.Card>
-    </S.Container>
-  );
+export type ChatRequestBubbleProps = {
+  messagePayload: {
+    chatMessageType: string;
+    meetingTime: string;
+    meetingPlace: string;
+    matchingId: number;
+  };
+  onAccept: (matchingId: number, accepted: boolean) => void;
+  onReject: (matchingId: number, accepted: boolean) => void;
 };
 
-export default MatchingBubble;
+export default function ChatRequestBubble({
+  messagePayload,
+  onAccept,
+  onReject,
+}: ChatRequestBubbleProps) {
+  console.log(messagePayload);
+  const isoTime = messagePayload.meetingTime;
+  const datePart = isoTime.split('T')[0];
+  const timePart = isoTime.split('T')[1]?.split(':').slice(0, 2).join(':');
+  const { meetingPlace } = messagePayload;
+  const messageType = messagePayload.chatMessageType;
+  return (
+    <S.Card>
+      <S.HeaderWrapper>
+        <S.Header>매칭 신청</S.Header>
+      </S.HeaderWrapper>
+      <S.Content>
+        <S.Row>
+          <S.Label>날짜:</S.Label>
+          <S.Value>{datePart}</S.Value>
+        </S.Row>
+        <S.Row>
+          <S.Label>시간:</S.Label>
+          <S.Value>{timePart}</S.Value>
+        </S.Row>
+        <S.Row>
+          <S.Label>장소:</S.Label>
+          <S.Value>{meetingPlace}</S.Value>
+        </S.Row>
+      </S.Content>
+      <S.ButtonWrapper>
+        {messageType === 'MATCHING_REQUEST' ? (
+          <div style={{ display: 'flex', gap: 8 }}>
+            <BubbleMessageButton
+              variant="primary"
+              onClick={() => onAccept(messagePayload.matchingId, true)}
+            >
+              수락
+            </BubbleMessageButton>
+            <BubbleMessageButton
+              variant="secondary"
+              onClick={() => onReject(messagePayload.matchingId, false)}
+            >
+              거절
+            </BubbleMessageButton>
+          </div>
+        ) : (
+          <BubbleMessageButton
+            variant="secondary"
+            onClick={() => onReject(messagePayload.matchingId, false)}
+          >
+            매칭 조회
+          </BubbleMessageButton>
+        )}
+      </S.ButtonWrapper>
+    </S.Card>
+  );
+}
