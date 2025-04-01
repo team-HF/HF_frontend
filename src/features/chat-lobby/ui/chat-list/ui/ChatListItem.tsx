55038ed7f0d9c96ae836ca3b5f3c5ecd152df5ee
Namhyun <jenple9709@gmail.com>
2025-04-01 21:25:35 +0900

    feat: 채팅방 클릭 시 해당 채팅방 navigate 추가

--- src/features/chat-lobby/ui/chat-list/ui/ChatListItem.tsx
+++ src/features/chat-lobby/ui/chat-list/ui/ChatListItem.tsx
@@ -8,12 +8,19 @@ import { ChatContent } from '../../../model/chat-lobby.types';
 import { getSgisApiAccessToken } from '../../../../../shared/api/getSgisApiAccessToken';
 import { getSgisLocation } from '../../../../../shared/api/getSgisLocation';
 import { useGetDate } from '../../../../../shared/utils/useGetDate';
+import { useNavigate } from 'react-router-dom';
 
 type ChatListItemProps = {
   chat: ChatContent;
 };
 
 export default function ChatListItem({ chat }: ChatListItemProps) {
+  const navigate = useNavigate();
+
+  const navigateToChatRoom = (chatRoomId: number) => {
+    navigate(`/chat/${chatRoomId}`);
+  };
+
   const [location, setLocation] = useState('');
   const { data: opponentInfo } = useGetMatchingUserInfo(
     chat.opponentParticipantId
@@ -62,57 +69,72 @@ export default function ChatListItem({ chat }: ChatListItemProps) {
   }, []);
 
   return (
-    <S.ListWrapper key={chat.chatroomId}>
-      <S.ProfileImageWrapper>
-        <S.ProfileImage
-          src={
-            opponentInfo?.profileImageUrl ||
-            chat.opponentParticipantProfileImageUrl ||
-            '/svg/default-profile-icon.svg'
-          }
-          alt="profile-icon"
-        />
-      </S.ProfileImageWrapper>
-      <S.MainWrapper>
-        <S.TextWrapper>
-          <S.StyledNickname>
-            {opponentInfo?.nickname || chat.opponentParticipantNickname}
-          </S.StyledNickname>
-          <S.StyledLocation>{location}</S.StyledLocation>
-          <S.StyledTimeStamp>{useGetDate(chat.chatSentTime)}</S.StyledTimeStamp>
-        </S.TextWrapper>
-        <S.ChatDescriptionWrapper>
-          <S.StyledChat>{chat.lastChatMessage}</S.StyledChat>
-        </S.ChatDescriptionWrapper>
-      </S.MainWrapper>
-      <S.SubWrapper>
-        <S.StyledMenuDotIcon
-          src="/svg/menu-dot-icon.svg"
-          alt="menu-dot-icon"
-          className="menu-dot"
-          onClick={toggleDropdown}
-        />
-        {isDropdownActive && (
-          <div className="dropdown" onClick={(e) => e.stopPropagation()}>
-            <ChatListDropdown
-              setIsOpenModal={setIsOpenModal}
-              setActiveDropdown={setIsDropdownActive}
-            />
-          </div>
-        )}
-        <S.MatchingCardWrapper>
-          <OnGoingMatchCard />
-        </S.MatchingCardWrapper>
-      </S.SubWrapper>
+    <>
+      <S.ListWrapper
+        key={chat.chatroomId}
+        onClick={(e) => {
+          e.stopPropagation();
+          navigateToChatRoom(chat.chatroomId);
+        }}
+      >
+        <S.ProfileImageWrapper>
+          <S.ProfileImage
+            src={
+              opponentInfo?.profileImageUrl ||
+              chat.opponentParticipantProfileImageUrl ||
+              '/svg/default-profile-icon.svg'
+            }
+            alt="profile-icon"
+          />
+        </S.ProfileImageWrapper>
+        <S.MainWrapper>
+          <S.TextWrapper>
+            <S.StyledNickname>
+              {opponentInfo?.nickname || chat.opponentParticipantNickname}
+            </S.StyledNickname>
+            <S.StyledLocation>{location}</S.StyledLocation>
+            <S.StyledTimeStamp>
+              {useGetDate(chat.chatSentTime)}
+            </S.StyledTimeStamp>
+          </S.TextWrapper>
+          <S.ChatDescriptionWrapper>
+            <S.StyledChat>{chat.lastChatMessage}</S.StyledChat>
+          </S.ChatDescriptionWrapper>
+        </S.MainWrapper>
+        <S.SubWrapper>
+          <S.StyledMenuDotIcon
+            src="/svg/menu-dot-icon.svg"
+            alt="menu-dot-icon"
+            className="menu-dot"
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleDropdown();
+            }}
+          />
+          {isDropdownActive && (
+            <div className="dropdown" onClick={(e) => e.stopPropagation()}>
+              <ChatListDropdown
+                setIsOpenModal={setIsOpenModal}
+                setActiveDropdown={setIsDropdownActive}
+              />
+            </div>
+          )}
+          <S.MatchingCardWrapper>
+            <OnGoingMatchCard />
+          </S.MatchingCardWrapper>
+        </S.SubWrapper>
+      </S.ListWrapper>
       {isOpenModal && (
         <>
           <S.ModalOverlay />
-          <ChatConfirmModal
-            onConfirm={() => setIsOpenModal(false)}
-            onCancel={() => setIsOpenModal(false)}
-          />
+          <div>
+            <ChatConfirmModal
+              onConfirm={() => setIsOpenModal(false)}
+              onCancel={() => setIsOpenModal(false)}
+            />
+          </div>
         </>
       )}
-    </S.ListWrapper>
+    </>
   );
 }
