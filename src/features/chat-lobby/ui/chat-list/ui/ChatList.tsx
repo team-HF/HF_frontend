0864541bcb9c959d275ddd012ff0609421f788d0
Namhyun <jenple9709@gmail.com>
2025-04-01 21:09:35 +0900

    feat: 하단 gnb바 추가

--- src/features/chat-lobby/ui/chat-list/ui/ChatList.tsx
+++ src/features/chat-lobby/ui/chat-list/ui/ChatList.tsx
@@ -1,6 +1,7 @@
 import * as S from './chat-list.style';
 import { ChatContent } from '../../../model/chat-lobby.types';
 import ChatListItem from './ChatListItem';
+import PageForm from '../../../../../shared/ui/page-form/PageForm';
 
 type ChatListProps = {
   chatListData: ChatContent[];
@@ -8,10 +9,12 @@ type ChatListProps = {
 
 export default function ChatList({ chatListData }: ChatListProps) {
   return (
-    <S.Container>
-      {chatListData.map((chat) => (
-        <ChatListItem key={chat.chatroomId} chat={chat} />
-      ))}
-    </S.Container>
+    <PageForm isGNB={true} isFooter={false}>
+      <S.Container>
+        {chatListData.map((chat) => (
+          <ChatListItem key={chat.chatroomId} chat={chat} />
+        ))}
+      </S.Container>
+    </PageForm>
   );
 }
